6301d70ad482ad6f2b3e788f3f7cf04bccf33d9b
moon <childe_mu@163.com>
2020-09-24 23:35:52 +0800

    添加延迟队列其他实现测试

--- src/main/java/com/koala/App.java
+++ src/main/java/com/koala/App.java
@@ -1,13 +1,18 @@
 package com.koala;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+@EnableScheduling
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
